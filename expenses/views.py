f7c00c40483165e4525d83adba77b10a1bd5668d
Marek <marekdrag@gmail.com>
2023-04-17 14:41:45 +0200

    Added date search functionality to ExpenseListView in views.py and expense_list.html template
    In views.py:
    - Added logic in ExpenseListView to filter expenses by date (from and/or to) based on the values entered in the search form.
    - Updated the get_context_data() method to include the date filter in the queryset.
    
    In expense_list.html template:
    - Added date input fields "from" and "to" to the search form, along with a "search" button.
    - Updated the form's action attribute to ensure that the date filter values are included in the URL parameters.
    - Updated the table to display the date column in the expense list.
    
    These changes allow users to search for expenses based on date range and view the filtered results in the expense list view. The form state is preserved after page refresh or when generating search results.

--- expenses/views.py
+++ expenses/views.py
@@ -3,6 +3,7 @@ from django.views.generic.list import ListView
 from .forms import ExpenseSearchForm
 from .models import Expense, Category
 from .reports import summary_per_category
+from django.utils import timezone
 
 
 class ExpenseListView(ListView):
@@ -23,6 +24,22 @@ class ExpenseListView(ListView):
             object_list=queryset,
             summary_per_category=summary_per_category(queryset),
             **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        date_from = self.request.GET.get('from')
+        date_to = self.request.GET.get('to')
+
+        if date_from and date_to:
+            queryset = queryset.filter(date__range=(date_from, date_to))
+        elif date_from:
+            queryset = queryset.filter(date__gte=date_from)
+        elif date_to:
+            queryset = queryset.filter(date__lte=date_to)
+
+        return queryset
+
 
 class CategoryListView(ListView):
     model = Category
